fd1431adb3ad1891682727fd8ea7254bef818b4f
kartoFlane <tbachminski@gmail.com>
2022-12-27 02:44:55 +0100

    Add tests for PKG writer

---
+++ tests/pkg_writer.rs
@@ -0,0 +1,70 @@
+#[cfg(test)]
+mod test_pkg_writer {
+    use std::path::Path;
+
+    use ftldat::{Package, pkg_package};
+
+    const TEST_DAT_PATH: &str = "./tests-resources/test.pkg";
+
+    #[test]
+    fn writer_should_create_file_on_disk_if_missing() {
+        // Prepare
+        let mut package = Package::new();
+        package.put_entry_from_string("test", "test123");
+
+        let tmp_file = tempfile::NamedTempFile::new().unwrap();
+        let tmp_path = tmp_file.path().to_str().unwrap();
+
+        // Execute
+        let result = pkg_package::write_package_to_path(package, tmp_path);
+
+        // Check
+        assert!(result.is_ok());
+        assert!(tmp_file.path().exists());
+        assert_eq!(51, tmp_file.as_file().metadata().unwrap().len());
+    }
+
+    #[test]
+    fn writer_should_update_file_on_disk_if_exists() {
+        // Prepare
+        let tmp_file = tempfile::NamedTempFile::new().unwrap();
+        let tmp_path = tmp_file.path().to_str().unwrap();
+
+        std::fs::copy(Path::new(TEST_DAT_PATH), tmp_file.path())
+            .expect("failed to copy test.dat for testing");
+
+        let mut package = Package::new();
+        package.put_entry_from_string("test", "test123");
+
+        // Execute
+        let result = pkg_package::write_package_to_path(package, tmp_path);
+
+        // Check
+        assert!(result.is_ok());
+        assert!(tmp_file.path().exists());
+        assert_eq!(51, tmp_file.as_file().metadata().unwrap().len());
+    }
+
+    #[test]
+    fn entry_order_should_be_retained_between_writes() {
+        // Prepare
+        let tmp_file = tempfile::NamedTempFile::new().unwrap();
+        let tmp_path = tmp_file.path().to_str().unwrap();
+
+        let package = pkg_package::read_from_path(TEST_DAT_PATH).unwrap();
+        let order_before_write = package.inner_paths();
+
+        // Execute
+        let result = pkg_package::write_package_to_path(package, tmp_path);
+        println!("{}", tmp_path);
+        assert!(result.is_ok());
+        let package = pkg_package::read_from_path(tmp_path).unwrap();
+        let order_after_write = package.inner_paths();
+
+        // Check
+        assert_eq!(order_before_write.len(), order_after_write.len());
+        assert_eq!(order_before_write[0], order_after_write[0]);
+        assert_eq!(order_before_write[1], order_after_write[1]);
+        assert_eq!(order_before_write[2], order_after_write[2]);
+    }
+}
